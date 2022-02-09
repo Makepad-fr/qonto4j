e635fd0534370f646680ea3559954aaa39080b5f
kaanyagci <kaan.yagci@clique.paris>
2022-02-09 12:33:41 +0100

    feat(external-trasfers): ExternalTransfer record updated
    new static method added to get the BodyHandler related to the record

--- src/main/java/io/makepad/qonto4j/ExternalTransfer.java
+++ src/main/java/io/makepad/qonto4j/ExternalTransfer.java
@@ -3,6 +3,10 @@
 */
 package io.makepad.qonto4j;
 
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
+import java.net.http.HttpResponse.BodySubscriber;
+import java.net.http.HttpResponse.ResponseInfo;
 import java.time.LocalDateTime;
 import java.util.Currency;
 import java.util.Date;
@@ -29,4 +33,18 @@ public record ExternalTransfer(
         LocalDateTime createdAt,
         LocalDateTime completedAt,
         LocalDateTime processedAt,
-        String transactionId) {}
+        String transactionId) {
+
+  /**
+   * Get the body handler for external transfers
+   * @return The HttpResponse.BodyHandler for ExternalTransfer
+   */
+  static HttpResponse.BodyHandler<ExternalTransfer> bodyHandler() {
+    return new BodyHandler<ExternalTransfer>() {
+      @Override
+      public BodySubscriber<ExternalTransfer> apply(ResponseInfo responseInfo) {
+        return null;
+      }
+    }
+  }
+}
